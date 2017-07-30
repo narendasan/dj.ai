eddfac592b1bca02a679314a2a59f4218eec116b
root <mxfeinberg42@gmail.com>
2017-07-30 04:19:54 +0000

    Added Spectrogram Functionality

--- __init__.py
+++ __init__.py
@@ -1,11 +1,19 @@
 import sys
 import torch
 from sound_data import sound_data
+from scipy import signal
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+# Returns Frequency, Time, Signal
+def get_spectrogram(data, seg_size, overlap):
+  return signal.spectrogram(data, 
+                            nperseg = 8 * 1024,
+                            noverlap = 8 * (1024-512)) 
 
 def main():
   dset = sound_data()
   loader = torch.utils.data.DataLoader(dset, num_workers = 8)
-  print(len(next(iter(loader))[0][0][0]))
 
 
 
